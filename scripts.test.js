76d2cc568520c6c5cd268061ce4e868ad2efb13c
Tommy-83 <tommy.kosakowski@gmail.com>
2023-04-08 13:19:08 +0000

    jest testing

---
+++ scripts.test.js
@@ -0,0 +1,17 @@
+const { constant } = require('lodash');
+const shuffle = require('./scripts.js');
+
+//confirm shuffle function
+test('does shuffle function exists', () => {
+    expect(typeof shuffle) === ('function');
+})
+
+test('checking if shuffle function works ans shuffels cards', () => {
+    beforeEach(() => {
+        jest.spyOn(global.math, 'random').mockReturnValue();
+    });
+
+    afterEach(() => {
+        jest.spyOn(global.math, 'random').mockRestore();
+    })
+})
