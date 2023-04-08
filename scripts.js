2281e58336f96990144e5118c481c4ded3e03f4b
Tommy-83 <tommy.kosakowski@gmail.com>
2023-04-08 12:57:55 +0000

    starting js with flip card

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+  
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
